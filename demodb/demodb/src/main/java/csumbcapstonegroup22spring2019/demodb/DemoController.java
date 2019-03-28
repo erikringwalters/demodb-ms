f2827ec69fa0c96415e425bcc7ff859c7b616420
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-03-27 19:33:54 -0700

    setting up ihero repo interface

--- demodb/demodb/src/main/java/csumbcapstonegroup22spring2019/demodb/DemoController.java
+++ demodb/demodb/src/main/java/csumbcapstonegroup22spring2019/demodb/DemoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import csumbcapstonegroup22spring2019.demodb.entities.Hero;
@@ -16,6 +17,18 @@ public class DemoController {
     @GetMapping ("/allHeroes")
     public List<Hero> getAll() {
         List<Hero> result = demoRepository.findAll();
+        return result;
+    }
+
+    @GetMapping("/id/{id}")
+    public Hero getHeroById (@PathVariable String id) {
+        Hero result = demoRepository.findByRepoId(id);
+        return result;
+    }
+
+    @GetMapping("/power/{power}")
+    public List<Hero> getByPower (@PathVariable String power) {
+        List<Hero> result = demoRepository.findByPower(power);
         return result;
     }
 }
