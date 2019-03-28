5f0b7c6ee1f14c7c3c1e839d447a832ea47813a6
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-03-27 19:29:58 -0700

    setting up ihero repo interface

--- demodb/demodb/src/main/java/csumbcapstonegroup22spring2019/demodb/IDemoRepository.java
+++ demodb/demodb/src/main/java/csumbcapstonegroup22spring2019/demodb/IDemoRepository.java
@@ -1,12 +1,19 @@
 package csumbcapstonegroup22spring2019.demodb;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import csumbcapstonegroup22spring2019.demodb.entities.Hero;
 
 @Repository
 public interface IDemoRepository extends MongoRepository<Hero, String>{
-    
+    @Query (value = "{'id':?0}")
+    Hero findByRepoId(String id);
+
+    @Query (value = "{'powers.powers':?0}")
+    List<Hero> findByPower(String name);
 
 }
