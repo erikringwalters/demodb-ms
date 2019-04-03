68226e7f840cf781f3f81a91e8f91957c391ebc1
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-04-03 16:53:21 -0700

    removed lineCharts from demo files

--- src/main/java/csumbcapstonegroup22spring2019/demodb/IDemoRepository.java
+++ src/main/java/csumbcapstonegroup22spring2019/demodb/IDemoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import csumbcapstonegroup22spring2019.demodb.entities.hero.Hero;
-import csumbcapstonegroup22spring2019.demodb.entities.linechart.LineChart;
+// import csumbcapstonegroup22spring2019.demodb.entities.linechart.LineChart;
 
 @Repository
 public interface IDemoRepository extends MongoRepository<Hero, String>{
@@ -17,7 +17,7 @@ public interface IDemoRepository extends MongoRepository<Hero, String>{
     @Query (value = "{'powers.powers':?0}")
     List<Hero> findByPower(String name);
 
-    @Query (value = "{'chart.id':?0}")
-    LineChart findByLineChartId(String id);
+    // @Query (value = "{'chart.id':?0}")
+    // LineChart findByLineChartId(String id);
 
 }
