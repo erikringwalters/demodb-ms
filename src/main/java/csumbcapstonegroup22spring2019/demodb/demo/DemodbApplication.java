186f704f0369ce69972adb3bffc1986b051bb5e0
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-04-03 19:18:16 -0700

    disabled demodbApp main class

--- src/main/java/csumbcapstonegroup22spring2019/demodb/demo/DemodbApplication.java
+++ src/main/java/csumbcapstonegroup22spring2019/demodb/demo/DemodbApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemodbApplication.class, args);
-	}
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(DemodbApplication.class, args);
+	// }
 
 }
