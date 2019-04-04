957e481c0e244e3ccca23153bed5437b68cf6051
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-04-03 19:10:23 -0700

    baby steps

--- src/main/java/csumbcapstonegroup22spring2019/demodb/linechart/ILineChartRepository.java
+++ src/main/java/csumbcapstonegroup22spring2019/demodb/linechart/ILineChartRepository.java
@@ -1,16 +1,16 @@
-// package csumbcapstonegroup22spring2019.demodb.linechart;
+package csumbcapstonegroup22spring2019.demodb.linechart;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.data.mongodb.repository.MongoRepository;
-// import org.springframework.data.mongodb.repository.Query;
-// import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
-// import csumbcapstonegroup22spring2019.demodb.entities.linechart.LineChart;
+import csumbcapstonegroup22spring2019.demodb.entities.linechart.LineChart;
 
-// @Repository
-// public interface ILineChartRepository extends MongoRepository<LineChart, String>{
-//     @Query (value = "{'id':?0}")
-//     LineChart findByRepoId(String id);
+@Repository
+public interface ILineChartRepository extends MongoRepository<LineChart, String>{
+    @Query (value = "{'id':?0}")
+    LineChart findByRepoId(String id);
 
-// }
+}
--- src/main/java/csumbcapstonegroup22spring2019/demodb/linechart/LineChartDbSeeder.java
+++ src/main/java/csumbcapstonegroup22spring2019/demodb/linechart/LineChartDbSeeder.java
@@ -1,53 +1,50 @@
-// package csumbcapstonegroup22spring2019.demodb.linechart;
+package csumbcapstonegroup22spring2019.demodb.linechart;
 
-// import java.util.Arrays;
-// import java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-// import csumbcapstonegroup22spring2019.demodb.entities.linechart.LineChart;
-// import csumbcapstonegroup22spring2019.demodb.entities.linechart.Series;
+import csumbcapstonegroup22spring2019.demodb.entities.linechart.LineChart;
+import csumbcapstonegroup22spring2019.demodb.entities.linechart.Series;
 
 
 
-// @Component
-// public class LineChartDbSeeder implements CommandLineRunner {
-//     @Autowired
-//     ILineChartRepository lineChartRepo;
+@Component
+public class LineChartDbSeeder implements CommandLineRunner {
+    @Autowired
+    ILineChartRepository lineChartRepo;
 
-//     @Override
-//     public void run(String... args) throws Exception {
-//         String greenName = "Green";
-//         Series[] greenSeries = new Series[4];
-//         greenSeries[0] = new Series(5, 100);
-//         greenSeries[1] = new Series(10, 250);
-//         greenSeries[2] = new Series(15, 1500);
-//         greenSeries[3] = new Series(20, 750);
-//         greenSeries[4] = new Series(25, 500);
+    @Override
+    public void run(String... args) throws Exception {
+        String greenName = "Green";
+        Series[] greenSeries = new Series[4];
+        greenSeries[0] = new Series(5, 100);
+        greenSeries[1] = new Series(10, 250);
+        greenSeries[2] = new Series(15, 1500);
+        greenSeries[3] = new Series(20, 750);
+        greenSeries[4] = new Series(25, 500);
         
 
-//         LineChart greenLineChart = new LineChart(greenName, greenSeries);
+        LineChart greenLineChart = new LineChart(greenName, greenSeries);
 
-//         String redName = "Red";
-//         Series[] redSeries = new Series[4];
-//         redSeries[0] = new Series(5, 150);
-//         redSeries[1] = new Series(10, 2500);
-//         redSeries[2] = new Series(15, 150);
-//         redSeries[3] = new Series(20, 650);
-//         redSeries[4] = new Series(25, 50);
+        String redName = "Red";
+        Series[] redSeries = new Series[4];
+        redSeries[0] = new Series(5, 150);
+        redSeries[1] = new Series(10, 2500);
+        redSeries[2] = new Series(15, 150);
+        redSeries[3] = new Series(20, 650);
+        redSeries[4] = new Series(25, 50);
 
-//         LineChart redLineChart = new LineChart(redName, redSeries);
+        LineChart redLineChart = new LineChart(redName, redSeries);
 
-//          // delete db data
-//          lineChartRepo.deleteAll();
-//          // add db seeds
-//         List<LineChart> lineCharts = Arrays.asList(greenLineChart, redLineChart);
-//         lineChartRepo.saveAll(lineCharts);
+         // delete db data
+         lineChartRepo.deleteAll();
+         // add db seeds
+        List<LineChart> lineCharts = Arrays.asList(greenLineChart, redLineChart);
+        lineChartRepo.saveAll(lineCharts);
+    }
 
-
-
-//     }
-
-// }
+}
