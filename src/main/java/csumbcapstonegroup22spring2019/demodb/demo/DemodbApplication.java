a9932a2cfec93262f6740480fdd2a4e114dd230c
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-04-03 19:26:56 -0700

    enabled linechartdbApp. disabled demoDbApp

--- src/main/java/csumbcapstonegroup22spring2019/demodb/demo/DemodbApplication.java
+++ src/main/java/csumbcapstonegroup22spring2019/demodb/demo/DemodbApplication.java
@@ -1,15 +1,15 @@
-package csumbcapstonegroup22spring2019.demodb.demo;
+// package csumbcapstonegroup22spring2019.demodb.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// // import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-// @EnableEurekaClient
-@SpringBootApplication
-public class DemodbApplication {
+// // @EnableEurekaClient
+// @SpringBootApplication
+// public class DemodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemodbApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DemodbApplication.class, args);
+// 	}
 
-}
+// }
--- src/main/java/csumbcapstonegroup22spring2019/demodb/linechart/LineChartdbApplication.java
+++ src/main/java/csumbcapstonegroup22spring2019/demodb/linechart/LineChartdbApplication.java
@@ -1,15 +1,15 @@
-// package csumbcapstonegroup22spring2019.demodb.linechart;
+package csumbcapstonegroup22spring2019.demodb.linechart;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// // import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-// // @EnableEurekaClient
-// @SpringBootApplication
-// public class LineChartdbApplication {
+// @EnableEurekaClient
+@SpringBootApplication
+public class LineChartdbApplication {
 
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(LineChartdbApplication.class, args);
-// 	}
+	public static void main(String[] args) {
+		SpringApplication.run(LineChartdbApplication.class, args);
+	}
 
-// }
+}
